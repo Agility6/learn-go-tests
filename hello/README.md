81371226050e8a075f5be665ffc7fb0066b57b41
Agility6 <agility1013@gmail.com>
2023-10-28 23:39:24 +0800

    feat: hello world

---
+++ hello/README.md
@@ -0,0 +1,11 @@
+## Hello World
+
+#### What to learn
+
+- 如何写测试
+
+- 声明函数，带参数的和返回值类型
+
+- `if` `const` `switch`
+
+- 常量和变量
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	helloPrefix       = "Hello, "
+	spanisHelloPrefix = "Hola, "
+	frenchHelloPrefix = "French "
+)
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+// function private
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanisHelloPrefix
+	default:
+		prefix = helloPrefix
+	}
+	return
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello, World")
 }
